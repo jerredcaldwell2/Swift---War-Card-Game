7945bcfa13d78735361dbb092f661fdd30b657b9
jerredcaldwell <jerredc09@gmail.com>
2022-08-22 16:08:02 -0500

    Initial Commit

---
+++ card_game/card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_gameApp.swift
+//  card_game
+//
+//  Created by Jerred Caldwell on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
